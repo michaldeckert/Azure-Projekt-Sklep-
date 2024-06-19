483bc8cfbe1e11d7574b17f8693a923bcfa8028e
michaldeckert <157825383+michaldeckert@users.noreply.github.com>
2024-06-19 12:14:01 +0200

    Update variables.py

--- variables.py
+++ variables.py
@@ -2,9 +2,5 @@ import os
 # AZURE_SQL_CONNECTIONSTRING = 'Driver={ODBC Driver 18 for SQL Server};Server=tcp:sqlserversklepprojekt.database.windows.net,1433;Database=sklepMuzyczny;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30'
 usernameV = os.getenv('DATABASE_USERNAME')
 passwordV = os.getenv('DATABASE_PASSWORD')
-
-if not usernameV or not passwordV:
-    raise ValueError("Zmienne środowiskowe DATABASE_URL lub SECRET_KEY nie zostały ustawione!")
-
-print(f"USERNAME: {usernameV}")
-print(f"PASSWORD: {passwordV}")
+databaseV = os.getenv('DATABASE_NAME')
+serverV = os.getenv('DATABASE_SERVER')
