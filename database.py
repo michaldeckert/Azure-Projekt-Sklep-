42c46c3e37565367abe98fab6909c42a6416b0c0
michaldeckert <157825383+michaldeckert@users.noreply.github.com>
2024-06-19 02:59:56 +0200

    Add files via upload

---
+++ database.py
@@ -0,0 +1,22 @@
+import urllib
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from variables import usernameV, passwordV
+
+server = "sqlserversklepprojekt.database.windows.net"
+database = "sklepMuzyczny"
+username = usernameV
+password = passwordV
+
+driver = '{ODBC Driver 18 for SQL Server}'
+
+# odbc_str = 'Driver={ODBC Driver 18 for SQL Server};Server=tcp:sqlserversklepprojekt.database.windows.net,1433;Database=sklepMuzyczny;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30'
+odbc_str = 'DRIVER='+driver+';SERVER='+server+';PORT=1433;UID='+username+';DATABASE='+ database + ';PWD='+ password
+connect_str = 'mssql+pyodbc:///?odbc_connect=' + urllib.parse.quote_plus(odbc_str)
+
+print(connect_str)
+
+engine = create_engine(connect_str)
+SessionLocal = sessionmaker(autocommit=False, autoflush = False, bind = engine)
+
+Base = declarative_base()
---
+++ sklepapp.py
@@ -0,0 +1,79 @@
+from typing import Optional
+from fastapi import FastAPI, Request, Header, Depends, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+
+from database import SessionLocal, engine
+import models
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get('/index/', response_class=HTMLResponse)
+async def wyposazenie(
+    request: Request, 
+    hx_request: Optional[str] = Header(None),
+    db: Session = Depends(get_db)
+    ): 
+    wyposazenie = db.query(models.Wyposazenie).all()
+    context = {'request': request, 'wyposazenie': wyposazenie}
+    return templates.TemplateResponse("index.html", context)
+
+@app.post('/delete/', response_class=HTMLResponse)
+async def usun_wyposazenie(
+    request: Request,
+    id: int = Form(...),
+    hx_request: Optional[str] = Header(None),
+    db: Session = Depends(get_db)
+    ):
+    #Usunięcie rekordu z bazy danych
+    wyposazenie = db.query(models.Wyposazenie).filter(models.Wyposazenie.id == id).first()
+    if wyposazenie:
+        db.delete(wyposazenie)
+        db.commit()
+    wyposazenie = db.query(models.Wyposazenie).all()
+    context = {'request': request, 'wyposazenie': wyposazenie}
+    if hx_request:
+        return templates.TemplateResponse("table.html", context)
+    
+
+@app.post('/add/', response_class=HTMLResponse)
+async def dodaj_wyposazenie(
+    request: Request,
+    nazwa: str = Form(...),
+    wykonawca: str = Form(...),
+    gatunek: str = Form(...),
+    nosnik: str = Form(...),
+    wydawnictwo: str = Form(...),
+    wydano: str = Form(...),
+    hx_request: Optional[str] = Header(None),
+    db: Session = Depends(get_db)
+    ):
+    # Dodanie nowego rekordu do bazy danych
+    nowe_wyposazenie = models.Wyposazenie(
+        nazwa=nazwa,
+        wykonawca=wykonawca,
+        gatunek=gatunek,
+        nosnik=nosnik,
+        wydawnictwo=wydawnictwo,
+        wydano=wydano
+    )
+    db.add(nowe_wyposazenie)
+    db.commit()
+    db.refresh(nowe_wyposazenie)
+    
+    # Pobierz zaktualizowaną listę wyposazenia
+    wyposazenie = db.query(models.Wyposazenie).all()
+    context = {'request': request, 'wyposazenie': wyposazenie}
+    #Odpowiedz odpowiednim szablonem tylko dla tabeli
+    if hx_request:
+        return templates.TemplateResponse("table.html", context)
