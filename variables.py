1a6c4886c95704aeed0b8f77e459dc9fe972de24
michaldeckert <157825383+michaldeckert@users.noreply.github.com>
2024-06-19 12:14:15 +0200

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,5 +1,4 @@
 import os
-# AZURE_SQL_CONNECTIONSTRING = 'Driver={ODBC Driver 18 for SQL Server};Server=tcp:sqlserversklepprojekt.database.windows.net,1433;Database=sklepMuzyczny;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30'
 usernameV = os.getenv('DATABASE_USERNAME')
 passwordV = os.getenv('DATABASE_PASSWORD')
 databaseV = os.getenv('DATABASE_NAME')
