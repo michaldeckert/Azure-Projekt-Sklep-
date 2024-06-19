db7d3268a078fde65f2a9f0c9a219b644ec4cf31
michaldeckert <157825383+michaldeckert@users.noreply.github.com>
2024-06-19 10:45:56 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import urllib
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from variables import usernameV, passwordV
+from sklepMuzycznyApp import usernameV, passwordV
 
 server = "sqlserversklepprojekt.database.windows.net"
 database = "sklepMuzyczny"
