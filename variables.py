3cc1fa562ea549d40eb33856407656c6ea6f9333
michaldeckert <157825383+michaldeckert@users.noreply.github.com>
2024-06-19 10:57:11 +0200

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,3 @@
+AZURE_SQL_CONNECTIONSTRING = 'Driver={ODBC Driver 18 for SQL Server};Server=tcp:sqlserversklepprojekt.database.windows.net,1433;Database=sklepMuzyczny;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30'
+usernameV = "CloudSAda2e6518"
+passwordV = "Chmura&Projekt350"
