dcd8b0ff2316ae312389c78230daf4206f5c643d
michaldeckert <157825383+michaldeckert@users.noreply.github.com>
2024-06-19 10:58:16 +0200

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,3 +1,3 @@
 AZURE_SQL_CONNECTIONSTRING = 'Driver={ODBC Driver 18 for SQL Server};Server=tcp:sqlserversklepprojekt.database.windows.net,1433;Database=sklepMuzyczny;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30'
-usernameV = "CloudSAda2e6518"
-passwordV = "Chmura&Projekt350"
+usernameV = DATABASE_USERNAME
+passwordV = DATABASE_PASSWORD
